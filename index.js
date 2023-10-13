ac505f267f7f55e18f380ac757c9efd636ce0a12
Leen4k <110277798+Leen4k@users.noreply.github.com>
2023-10-13 16:19:00 +0700

    fetching data using graphql

---
+++ index.js
@@ -0,0 +1,77 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import data from './_db.js';
+import { typeDefs } from './schema.js';
+
+
+const resolvers = {
+    Query: {
+        games() {
+            return data.games
+        },
+        game(_,args) {
+            return data.games.find(
+                game => game.id === args.id
+            )
+        },
+        reviews() {
+            return data.reviews
+        },
+        review (_, args){
+            return data.reviews.find(
+                review => review.id === args.id
+            )
+        },
+        authors() {
+            return data.authors
+        },
+        author(_, args){
+            return data.authors.find(
+                author => author.id === args.id
+            )
+        }
+    },
+    Game : {
+        reviews(parent){
+            return data.reviews.filter(
+               (r) => (
+                r.game_id === parent.id
+               ))
+        }
+    },
+    Author : {
+        reviews(parent){
+            return data.reviews.filter(
+                (r) => (
+                 r.author_id === parent.id
+                ))
+        }
+    },
+    Review : {
+        author(parent){
+            return data.authors.find((a)=>(
+                a.id === parent.author_id
+            ))
+        },
+        game(parent){
+            return data.games.find((g)=>(
+                g.id === parent.game_id
+            ))
+        }
+    }
+}
+
+
+const server = new ApolloServer({
+    // type def are definition of different data that we wanna expose on the graph
+    typeDefs,
+    resolvers
+    // resolvers 
+})
+
+
+const {url} = await startStandaloneServer(server, {
+    listen: { port: 4000 }
+})
+
+console.log("server is running on port", 4000)
